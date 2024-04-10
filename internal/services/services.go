dc7e8fb81f88835e97f2586842a0f0712a58385e
John Alex <jangulovides@gmail.com>
2024-04-09 22:47:27 -0500

    Optimización de código

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"sync"
+	"time"
+
 	"github.com/Jhon-2801/sismos-api/internal/models"
 	"github.com/Jhon-2801/sismos-api/internal/repository"
 )
@@ -27,50 +30,104 @@ func (s *service) Count() (int, error) {
 }
 
 func (s *service) GetFeactures(limit, offset int) ([]*models.Feature, error) {
-	var featuresResponse []*models.Feature
 
-	features, err := s.repo.HttpGet(limit, offset)
+	geoJson, err := s.repo.HttpGet(limit, offset)
 	if err != nil {
 		return nil, err
 	}
 
+	//Persistir en la base de datos
+	features := parcerGeoJsonToEvents(geoJson)
 	err = s.repo.PostFectures(features)
 	if err != nil {
 		return nil, err
 	}
-	featuresDB, err := s.repo.GetFeactures(offset, limit)
+
+	//Obtener de la base de datos
+	featuresModels, err := s.repo.GetFeactures(offset, limit)
 	if err != nil {
 		return nil, err
 	}
-	for _, f := range featuresDB {
-		// Crear las coordenadas
-		coordinates := models.Coordinates{
-			Longitude: f.Longitude,
-			Latitude:  f.Latitude,
-		}
 
-		// Crear el modelo Feature
-		feature := &models.Feature{
-			ID:   f.ID,
-			Type: "feature",
-			Attributes: models.FeatureAttributes{
-				ExternalID:  f.EventID,
-				Magnitude:   f.Magnitude,
-				Place:       f.Place,
-				Time:        f.EventTime,
-				Tsunami:     f.Tsunami,
-				MagType:     f.MagType,
-				Title:       f.Title,
-				Coordinates: coordinates,
-			},
-			Links: struct {
-				ExternalURL string `json:"external_url"`
-			}{
-				ExternalURL: f.URL,
-			},
-		}
-		featuresResponse = append(featuresResponse, feature)
-	}
+	//Pasear para mandar model de respuesta
+	featuresResponse := parcerModelToResponse(featuresModels)
 
 	return featuresResponse, nil
+}
+
+func parcerGeoJsonToEvents(geoJson *models.GeoJSON) []*models.Events {
+	var (
+		features []*models.Events
+		mutex    sync.Mutex
+		wg       sync.WaitGroup
+	)
+	for _, f := range geoJson.Features {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			feature := &models.Events{
+				EventID:   f.ID,
+				Magnitude: f.Properties.Mag,
+				Place:     f.Properties.Place,
+				EventTime: f.Properties.Time.Time,
+				URL:       f.Properties.URL,
+				Tsunami:   f.Properties.Tsunami != 0,
+				MagType:   f.Properties.MagType,
+				Title:     f.Properties.Title,
+				Longitude: f.Geometry.Coordinates[0],
+				Latitude:  f.Geometry.Coordinates[1],
+				CreatedAt: time.Now(),
+			}
+			mutex.Lock()
+			features = append(features, feature)
+			mutex.Unlock()
+		}()
+	}
+	wg.Wait()
+	return features
+}
+
+func parcerModelToResponse(model []models.Events) []*models.Feature {
+	var (
+		featuresResponse []*models.Feature
+		mutex            sync.Mutex
+		wg               sync.WaitGroup
+	)
+	for _, f := range model {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			// Crear las coordenadas
+			coordinates := models.Coordinates{
+				Longitude: f.Longitude,
+				Latitude:  f.Latitude,
+			}
+
+			// Crear el modelo Feature
+			feature := &models.Feature{
+				ID:   f.ID,
+				Type: "feature",
+				Attributes: models.FeatureAttributes{
+					ExternalID:  f.EventID,
+					Magnitude:   f.Magnitude,
+					Place:       f.Place,
+					Time:        f.EventTime,
+					Tsunami:     f.Tsunami,
+					MagType:     f.MagType,
+					Title:       f.Title,
+					Coordinates: coordinates,
+				},
+				Links: struct {
+					ExternalURL string `json:"external_url"`
+				}{
+					ExternalURL: f.URL,
+				},
+			}
+			mutex.Lock()
+			featuresResponse = append(featuresResponse, feature)
+			mutex.Unlock()
+		}()
+	}
+	wg.Wait()
+	return featuresResponse
 }
